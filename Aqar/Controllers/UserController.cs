820ba5bcf555b391c1d832801f701188760ff435
Abdelaziz Shaban <zizoshaban0@gmail.com>
2022-01-29 23:09:51 +0200

    user controller was edited

--- Aqar/Controllers/UserController.cs
+++ Aqar/Controllers/UserController.cs
@@ -4,6 +4,10 @@ namespace Aqar.Controllers
 {
     public class UserController : Controller
     {
+        public IActionResult getstudent()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
