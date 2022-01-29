dd9ae3c1c1929884fcb462e03af6866ca8904a08
Abdelaziz Shaban <zizoshaban0@gmail.com>
2022-01-29 23:00:45 +0200

    user controller was added

---
+++ Aqar/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aqar.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
